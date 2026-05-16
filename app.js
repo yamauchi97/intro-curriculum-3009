7c879b9486bf5fb88b8156b775b7f7d55a00299e
yamauchi <272127211+yamauchi97@users.noreply.github.com.>
2026-05-16 12:00:19 +0900

    catコマンドの実装

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 const fs = require('node:fs');
 
 function cat(fileName) {
-  const content = fs.readFile(fileName, 'utf8', () => {});
-  console.log(content);
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+      return resolve(data);
+    });
+  });
+  promise.then((content) => {
+    console.log(content);
+  })
 }
 
 cat(process.argv[2]);
